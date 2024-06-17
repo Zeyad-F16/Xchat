f1780a4b479e7ca8f4262412f189d070ba201e26
Zeyad <farghlyzeyad9@gmail.com>
2024-06-17 06:03:53 +0300

    Auth & WebHook

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig =  {
+    providers: [
+      {
+        domain: "https://super-stinkbug-81.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig ;
