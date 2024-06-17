02e452de519e0104a60f1620d03ed4ea779879dd
Zeyad <farghlyzeyad9@gmail.com>
2024-06-17 23:46:03 +0300

    Send Text Messages

--- convex/schema.ts
+++ convex/schema.ts
@@ -20,7 +20,7 @@ export default defineSchema({
 
 	messages: defineTable({
 		conversation: v.id("conversations"),
-		sender:v.optional(v.string()), // should be string so that it doesn't throw errors in openai part ("ChatGPT")
+		sender:v.string(), // should be string so that it doesn't throw errors in openai part ("ChatGPT")
 		content: v.string(),
 		messageType: v.union(v.literal("text"), v.literal("image"), v.literal("video")),
 	}).index("by_conversation", ["conversation"]),
