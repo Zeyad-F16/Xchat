2c75ba3608a4faba57fdbaa286e82a21bd38d739
Zeyad <farghlyzeyad9@gmail.com>
2024-06-18 06:16:41 +0300

    Get Messages

--- convex/messages.ts
+++ convex/messages.ts
@@ -42,21 +42,47 @@ export const sendTextMessage = mutation({
 			conversation: args.conversation,
 			messageType: "text",
 		});
+	},
+});
 
-		// // TODO => add @gpt check later
-		// if (args.content.startsWith("@gpt")) {
-		// 	// Schedule the chat action to run immediately
-		// 	await ctx.scheduler.runAfter(0, api.openai.chat, {
-		// 		messageBody: args.content,
-		// 		conversation: args.conversation,
-		// 	});
-		// }
 
-		// if (args.content.startsWith("@dall-e")) {
-		// 	await ctx.scheduler.runAfter(0, api.openai.dall_e, {
-		// 		messageBody: args.content,
-		// 		conversation: args.conversation,
-		// 	});
-		// }
+export const getMessages = query({
+	args: {
+		conversation: v.id("conversations"),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) {
+			throw new Error("Unauthorized");
+		}
+
+		const messages = await ctx.db
+			.query("messages")
+			.withIndex("by_conversation", (q) => q.eq("conversation", args.conversation))
+			.collect();
+
+		const userProfileCache = new Map();
+
+		const messagesWithSender = await Promise.all(
+			messages.map(async (message) => {
+				let sender;
+				// Check if sender profile is in cache
+				if (userProfileCache.has(message.sender)) {
+					sender = userProfileCache.get(message.sender);
+				} else {
+					// Fetch sender profile from the database
+					sender = await ctx.db
+						.query("users")
+						.filter((q) => q.eq(q.field("_id"), message.sender))
+						.first();
+					// Cache the sender profile
+					userProfileCache.set(message.sender, sender);
+				}
+
+				return { ...message, sender };
+			})
+		);
+
+		return messagesWithSender;
 	},
 });
